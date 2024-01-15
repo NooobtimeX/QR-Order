33abe3edc0689cc6a637719f0cc9559bf52e6c87
varintron <varintron.bua@dome.tu.ac.th>
2024-01-15 19:47:20 +0700

    index home page

--- app.vue
+++ app.vue
@@ -1,7 +1,20 @@
 <template>
   <AppHeader />
+
+
+
+
   <div class="py-1 items-center px-2 mx-auto max-w-5xl">
     <NuxtPage />
   </div>
+
+
+
+
   <AppFooter />
 </template>
+<script>
+
+</script>
+
+<style ></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,5 @@
 export default defineNuxtConfig({
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: "th",
-      },
-      script: [
-        {
-          hid: "gtm-script",
-          innerHTML: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })( window,document,'script','dataLayer','GTM-MFDKPGZ2');
-          `,
-          type: "text/javascript",
-          charset: "utf-8",
-          async: true,
-        },
-      ],
-    },
-  },
+  
   devtools: {
     enabled: true,
     timeline: {
@@ -41,14 +20,6 @@ export default defineNuxtConfig({
     "nuxt-schema-org",
     "nuxt-simple-sitemap",
   ],
-  site: {
-    url: "http://www.rs-award.com",
-  },
-  robots: {
-    UserAgent: "*",
-    Allow: [],
-  },
-  sitemap: {
-    defaults: {},
-  },
+ 
+ 
 });
