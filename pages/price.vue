5f0da0f0eec83611a152a53d6aec9068a5fa414b
varintron <varintron.bua@dome.tu.ac.th>
2024-01-18 23:41:16 +0700

    price login UI

---
+++ pages/price.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="mt-10">
+		<div>
+			<h2>เลือกแพ๊คเกจที่เหมาะกับร้านคุณ  &nbsp;
+				<button type="button"
+					class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">รายเดือน</button>
+				<button type="button"
+					class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">รายปี</button>
+			
+				</h2>
+				<div class="pt-4" ></div>
+			<hr class="my-2 border-b-4 border-blue-500 w-full " />
+			
+		</div>
+		<!-- เลือกโปร -->
+            <div class="pt-4">
+				<div class="flex justify-between ">
+                     <button class="h-auto w-64 bg-blue-400 rounded-lg">Starter</button>
+					 <button class="h-auto w-64 bg-blue-400 rounded-lg">Standard</button>
+					 <button class="h-auto w-64 bg-blue-400 rounded-lg">Pro</button>
+				</div>
+			</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style ></style>
