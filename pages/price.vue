25b10d3e8107c56dd2742fe888778a1f846d8865
NooobtimeX <u2gstudio@gmail.com>
2024-01-22 09:23:24 +0700

    code spacing

--- pages/price.vue
+++ pages/price.vue
@@ -1,177 +1,226 @@
 <template>
-	<div class="mt-10">
-		<div>
-			<h2>เลือกแพ๊คเกจที่เหมาะกับคุณ &nbsp;
+  <div class="mt-10">
+    <div>
+      <h2>เลือกแพ๊คเกจที่เหมาะกับคุณ &nbsp;</h2>
+      <div class="pt-4"></div>
+      <hr class="my-2 border-b-4 border-blue-500 w-full" />
+    </div>
+    <!-- เลือกโปร -->
+    <div class="pt-4">
+      <div class="flex justify-center">
+        <!-- โปร-->
+        <div class="h-auto w-8/12 h-fit bg-gray-200 rounded-lg">
+          <div>
+            <p class="font-serif text-5xl text-center">Standard</p>
+            <p class="text-center text-base pt-2">
+              ตอบโจทย์ทุกความต้องการ กับระบบ POS
+              จัดการร้านค้าที่ครบและดีที่สุดจาก Qashier
+            </p>
+            <p class="text-center text-base pt-2">
+              ในราคาที่คุ้มค่า ให้คุณขยายธุรกิจได้อย่างง่ายดาย และสะดวก
+            </p>
+            <p class="text-center text-base pt-2">
+              ให้คุณจัดการและยกระดับร้านได้ตามใจฝัน
+            </p>
+          </div>
+          <!-- pic slide -->
+          <div>
+            <div
+              id="animation-carousel"
+              class="relative w-full"
+              data-carousel="static"
+            >
+              <!-- Carousel wrapper -->
+              <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                <!-- Item 1 -->
+                <div class="hidden duration-200 ease-linear" data-carousel-item>
+                  <img
+                    src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    alt="..."
+                  />
+                </div>
+                <!-- Item 2 -->
+                <div class="hidden duration-200 ease-linear" data-carousel-item>
+                  <img
+                    src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    alt="..."
+                  />
+                </div>
+                <!-- Item 3 -->
+                <div
+                  class="hidden duration-200 ease-linear"
+                  data-carousel-item="active"
+                >
+                  <img
+                    src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    alt="..."
+                  />
+                </div>
+                <!-- Item 4 -->
+                <div class="hidden duration-200 ease-linear" data-carousel-item>
+                  <img
+                    src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    alt="..."
+                  />
+                </div>
+                <!-- Item 5 -->
+                <div class="hidden duration-200 ease-linear" data-carousel-item>
+                  <img
+                    src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    alt="..."
+                  />
+                </div>
+              </div>
+              <!-- Slider controls -->
+              <button
+                type="button"
+                class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                data-carousel-prev
+              >
+                <span
+                  class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+                >
+                  <svg
+                    class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 1 1 5l4 4"
+                    />
+                  </svg>
+                  <span class="sr-only">Previous</span>
+                </span>
+              </button>
+              <button
+                type="button"
+                class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                data-carousel-next
+              >
+                <span
+                  class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+                >
+                  <svg
+                    class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 9 4-4-4-4"
+                    />
+                  </svg>
+                  <span class="sr-only">Next</span>
+                </span>
+              </button>
+            </div>
+          </div>
+          <!-- button -->
+          <div class="flex justify-around">
+            <div class="">
+              <span class="text-xs">บาท</span>
+              <span class="text-base">750</span>
+              <span class="text-xs">/เดือน.</span>
+            </div>
+            <div class="">
+              <span class="text-xs">บาท</span>
+              <span class="text-base">9000</span>
+              <span class="text-xs">/ปี.</span>
+            </div>
+          </div>
 
-			</h2>
-			<div class="pt-4"></div>
-			<hr class="my-2 border-b-4 border-blue-500 w-full " />
-
-		</div>
-		<!-- เลือกโปร -->
-		<div class="pt-4">
-			<div class="flex justify-center ">
-				<!-- โปร-->
-				<div class="h-auto w-8/12 h-fit bg-gray-200 rounded-lg">
-					<div>
-						<p class="font-serif text-5xl text-center">Standard</p>
-						<p class="text-center text-base pt-2 "> ตอบโจทย์ทุกความต้องการ กับระบบ POS
-							จัดการร้านค้าที่ครบและดีที่สุดจาก Qashier
-						</p>
-						<p class="text-center text-base pt-2 ">ในราคาที่คุ้มค่า ให้คุณขยายธุรกิจได้อย่างง่ายดาย และสะดวก</p>
-						<p class="text-center text-base pt-2 ">ให้คุณจัดการและยกระดับร้านได้ตามใจฝัน</p>
-					</div>
-					<!-- pic slide -->
-					<div>
-
-
-						<div id="animation-carousel" class="relative w-full" data-carousel="static">
-							<!-- Carousel wrapper -->
-							<div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-								<!-- Item 1 -->
-								<div class="hidden duration-200 ease-linear" data-carousel-item>
-									<img src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
-										class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-										alt="...">
-								</div>
-								<!-- Item 2 -->
-								<div class="hidden duration-200 ease-linear" data-carousel-item>
-									<img src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
-										class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-										alt="...">
-								</div>
-								<!-- Item 3 -->
-								<div class="hidden duration-200 ease-linear" data-carousel-item="active">
-									<img src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
-										class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-										alt="...">
-								</div>
-								<!-- Item 4 -->
-								<div class="hidden duration-200 ease-linear" data-carousel-item>
-									<img src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
-										class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-										alt="...">
-								</div>
-								<!-- Item 5 -->
-								<div class="hidden duration-200 ease-linear" data-carousel-item>
-									<img src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
-										class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-										alt="...">
-								</div>
-							</div>
-							<!-- Slider controls -->
-							<button type="button"
-								class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-								data-carousel-prev>
-								<span
-									class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-									<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-										xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-											stroke-width="2" d="M5 1 1 5l4 4" />
-									</svg>
-									<span class="sr-only">Previous</span>
-								</span>
-							</button>
-							<button type="button"
-								class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-								data-carousel-next>
-								<span
-									class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-									<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-										xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-											stroke-width="2" d="m1 9 4-4-4-4" />
-									</svg>
-									<span class="sr-only">Next</span>
-								</span>
-							</button>
-						</div>
-
-					</div>
-					<!-- button -->
-					<div class="flex justify-around">
-						<div class="">
-							<span class="text-xs">บาท</span>
-							<span class="text-base">750</span>
-							<span class="text-xs">/เดือน.</span>
-						</div>
-						<div class="">
-							<span class="text-xs">บาท</span>
-							<span class="text-base">9000</span>
-							<span class="text-xs">/ปี.</span>
-						</div>
-					</div>
-
-					<div class="flex justify-around">
-
-						<button type="button"
-							class=" w-32 h-14 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">รายเดือน</button>
-						<button type="button"
-							class="w-32 h-14 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">รายปี</button>
-
-					</div>
-				</div>
-
-			</div>
-		</div>
-	</div>
-	<!-- โชว์ฟีเจอ -->
-	<div class="pt-14">
-		<div>
-			<h2 class="flex justify-center mb-6">ฟีเจอร์ระบบที่สำหรับคุณ</h2>
-		</div>
-		<!-- การ์ดฟีเจอ -->
-		<div class="flex gap-6 " >
-
-
-			<div
-				class="block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-				<div class="relative overflow-hidden bg-cover bg-no-repeat">
-					<img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg" alt="" />
-				</div>
-				<div class="p-6">
-					<h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-						Card title
-					</h5>
-					<p class="text-base text-neutral-600 dark:text-neutral-200">
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</p>
-				</div>
-
-				
-			</div>
-			<div
-				class="block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-				<div class="relative overflow-hidden bg-cover bg-no-repeat">
-					<img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg" alt="" />
-				</div>
-				<div class="p-6">
-					<h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-						Card title
-					</h5>
-					<p class="text-base text-neutral-600 dark:text-neutral-200">
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</p>
-				</div>
-
-				
-			</div>
-
-
-
-
-		</div>
-	</div>
-	<div class="pt-10"></div>
+          <div class="flex justify-around">
+            <button
+              type="button"
+              class="w-32 h-14 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              รายเดือน
+            </button>
+            <button
+              type="button"
+              class="w-32 h-14 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              รายปี
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- โชว์ฟีเจอ -->
+  <div class="pt-14">
+    <div>
+      <h2 class="flex justify-center mb-6">ฟีเจอร์ระบบที่สำหรับคุณ</h2>
+    </div>
+    <!-- การ์ดฟีเจอ -->
+    <div class="flex gap-6">
+      <div
+        class="block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+      >
+        <div class="relative overflow-hidden bg-cover bg-no-repeat">
+          <img
+            class="rounded-t-lg"
+            src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
+            alt=""
+          />
+        </div>
+        <div class="p-6">
+          <h5
+            class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50"
+          >
+            Card title
+          </h5>
+          <p class="text-base text-neutral-600 dark:text-neutral-200">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <div
+        class="block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+      >
+        <div class="relative overflow-hidden bg-cover bg-no-repeat">
+          <img
+            class="rounded-t-lg"
+            src="https://tecdn.b-cdn.net/img/new/standard/city/062.jpg"
+            alt=""
+          />
+        </div>
+        <div class="p-6">
+          <h5
+            class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50"
+          >
+            Card title
+          </h5>
+          <p class="text-base text-neutral-600 dark:text-neutral-200">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="pt-10"></div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style ></style>
+<style></style>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,98 +1,153 @@
 <template>
-  <div class="pt-10 ">
-    <form class="mx-auto w-auto h-auto rounded-lg border border-gray-300 p-2" method="POST">
+  <div class="pt-10">
+    <form
+      class="mx-auto w-auto h-auto rounded-lg border border-gray-300 p-2"
+      method="POST"
+    >
       <h1 class="text-xl md:text-2xl font-bold text-center">
         Create your account
       </h1>
- <!-- เมล ชื่อ -->
- <div class="grid grid-cols-2 gap-2">
-    <div class="w-full">
-        <label for="email" class="block text-sm font-bold text-gray-700"><b>Email</b></label>
-        <input type="email" name="" id="" placeholder="Enter Email Address"
-            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white" />
-    </div>
+      <!-- เมล ชื่อ -->
+      <div class="grid grid-cols-2 gap-2">
+        <div class="w-full">
+          <label for="email" class="block text-sm font-bold text-gray-700"
+            ><b>Email</b></label
+          >
+          <input
+            type="email"
+            name=""
+            id=""
+            placeholder="Enter Email Address"
+            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white"
+          />
+        </div>
 
-    <div class="w-full">
-        <label for="fullname" class="block text-sm font-bold text-gray-700"><b>Full Name</b></label>
-        <input type="text" name="" id="" placeholder="First Last"
-            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white" />
-    </div>
-</div>
+        <div class="w-full">
+          <label for="fullname" class="block text-sm font-bold text-gray-700"
+            ><b>Full Name</b></label
+          >
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="First Last"
+            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white"
+          />
+        </div>
+      </div>
 
-<!-- เบอ วันเกิด -->
-<div class="flex space-x-4 mt-4">
-    <div class="w-full">
-        <label for="phonenum" class="block text-sm font-bold text-gray-700"><b>Phone Number</b></label>
-        <input type="text" name="" id="" placeholder="Enter Phone Number"
-            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white" />
-    </div>
+      <!-- เบอ วันเกิด -->
+      <div class="flex space-x-4 mt-4">
+        <div class="w-full">
+          <label for="phonenum" class="block text-sm font-bold text-gray-700"
+            ><b>Phone Number</b></label
+          >
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Phone Number"
+            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white"
+          />
+        </div>
 
-    <div class="w-full">
-        <label for="day" class="block text-sm font-bold text-gray-700"><b>Day</b></label>
-        <select name="" id=""
-            class="w-40 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+        <div class="w-full">
+          <label for="day" class="block text-sm font-bold text-gray-700"
+            ><b>Day</b></label
+          >
+          <select
+            name=""
+            id=""
+            class="w-40 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+          >
             <option value="" disabled selected>Select Day</option>
             <!-- Add options for days here -->
             <!-- Example: -->
             <option value="1">1</option>
             <option value="2">2</option>
             <!-- ... continue for all days -->
-        </select>
-    </div>
+          </select>
+        </div>
 
-    <div class="w-full">
-        <label for="month" class="block text-sm font-bold text-gray-700"><b>Month</b></label>
-        <select name="" id=""
-            class="w-40 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+        <div class="w-full">
+          <label for="month" class="block text-sm font-bold text-gray-700"
+            ><b>Month</b></label
+          >
+          <select
+            name=""
+            id=""
+            class="w-40 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+          >
             <option value="" disabled selected>Select Month</option>
             <!-- Add options for months here -->
             <!-- Example: -->
             <option value="January">January</option>
             <option value="February">February</option>
             <!-- ... continue for all months -->
-        </select>
-    </div>
+          </select>
+        </div>
 
-    <div class="w-full">
-        <label for="year" class="block text-sm font-bold text-gray-700"><b>Year</b></label>
-        <select name="" id=""
-            class="w-40 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+        <div class="w-full">
+          <label for="year" class="block text-sm font-bold text-gray-700"
+            ><b>Year</b></label
+          >
+          <select
+            name=""
+            id=""
+            class="w-40 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+          >
             <option value="" disabled selected>Select Year</option>
             <!-- Add options for years here -->
             <!-- Example: -->
             <option value="2022">2022</option>
             <option value="2023">2023</option>
             <!-- ... continue for all years -->
-        </select>
-    </div>
-</div>
+          </select>
+        </div>
+      </div>
 
-<!-- พาส -->
-<div class="grid grid-cols-1 mt-4">
-    <div class="w-full">
-        <label for="Password" class="block text-sm font-bold text-gray-700"><b>Password</b></label>
-        <input type="password" name="" id="" placeholder="Enter Password"
-            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white" />
-    </div>
-    <div class="w-full">
-        <label for="Password" class="block text-sm font-bold text-gray-700 "><b>Re-Password</b></label>
-        <input type="password" name="" id="" placeholder="Enter Password"
-            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white" />
-    </div>
-</div>
+      <!-- พาส -->
+      <div class="grid grid-cols-1 mt-4">
+        <div class="w-full">
+          <label for="Password" class="block text-sm font-bold text-gray-700"
+            ><b>Password</b></label
+          >
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="Enter Password"
+            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white"
+          />
+        </div>
+        <div class="w-full">
+          <label for="Password" class="block text-sm font-bold text-gray-700"
+            ><b>Re-Password</b></label
+          >
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="Enter Password"
+            class="w-72 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 bg-white"
+          />
+        </div>
+      </div>
 
       <div class="mx-auto text-center">
         <button
           class="mx-auto text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-6 py-2 mt-2 text-center"
-          type="submit">
+          type="submit"
+        >
           Sign up
         </button>
       </div>
     </form>
-  </div >
-<div class="pb-10"></div>
+  </div>
+  <div class="pb-10"></div>
 </template>
 <style>
 input {
   @apply bg-black rounded-lg p-1 m-1;
-}</style>
+}
+</style>
