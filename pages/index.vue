f9419fc5a6cb970e1ade25f229501a21e6934d42
varintron <varintron.bua@dome.tu.ac.th>
2024-02-10 15:27:21 +0700

    home
    finish ver

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,175 @@
-<template>home page</template>
+<template>
+    <!--บน-->
+    <div>
+        <div class="overflow-hidden bg-white py-24 sm:py-32">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <div
+                    class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                    <div class="lg:pr-8 lg:pt-4">
+                        <div class="lg:max-w-lg">
+                            <h2 class="text-xl font-semibold leading-7 text-indigo-600">QR Food</h2>
+                            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                                ระบบการสั่งซื้อด้วยคิวอาร์โค้ด
+                            </p>
+                            <p class="mt-6 text-lg leading-8 text-gray-600">
+                                การสั่งซื้อด้วยคิวอาร์โค้ดช่วยให้ลูกค้ากำหนดการสั่งซื้อได้อย่างง่ายดายด้วยอุปกรณ์มือถือของตนเอง<br>
+                                เพิ่มประสิทธิภาพการบริการและพัฒนาประสบการณ์ของลูกค้า</p>
+
+                        </div>
+                    </div>
+                    <img src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                        alt="Product screenshot"
+                        class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                        width="2432" height="1442">
+                </div>
+            </div>
+        </div>
+        <!-- กลาง-->
+        <div>
+            <div class="bg-white ">
+                <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div class="mx-auto max-w-2xl lg:text-center">
+                        <h2 class="text-3xl font-semibold leading-7 text-indigo-600">ข้อดีที่มากกว่า</h2>
+
+                    </div>
+                    <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                        <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                            <div class="relative pl-16">
+                                <dt class="text-base font-semibold leading-7 text-gray-900">
+                                    <span
+                                        class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-gray-300">&#10227;</span>
+
+                                    ประสิทธิภาพการทำงานเพิ่มมากขึ้น
+                                </dt>
+                                <dd class="mt-2 text-base leading-7 text-gray-600">
+                                    ทุกออเดอร์ที่ได้รับจะส่งตรงเข้าเว็บแอพพลิเคชันและครัวของคุณ
+                                    ให้การทำงานอย่างราบรื่นไร้รอยต่อ</dd>
+                            </div>
+                            <div class="relative pl-16">
+                                <dt class="text-base font-semibold leading-7 text-gray-900">
+
+                                    <span
+                                        class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-gray-300">&#9881;</span>
+
+
+                                    เพิ่ม แก้ไข และจัดการได้อย่างครบเครื่อง
+                                </dt>
+                                <dd class="mt-2 text-base leading-7 text-gray-600">เมนูออนไลน์ของร้านถูกร่วมเข้ากับระบบ
+                                    Qashier POS และสามารถปรับเปลี่ยนแก้ไขได้อย่างเต็มที่ ผ่านระบบหลังบ้าน</dd>
+                            </div>
+                            <div class="relative pl-16">
+                                <dt class="text-base font-semibold leading-7 text-gray-900">
+                                    <span
+                                        class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-gray-300">&#10084;</span>
+
+                                    ประสบการณ์ของลูกค้าที่ดียิ่งขึ้น
+                                </dt>
+                                <dd class="mt-2 text-base leading-7 text-gray-600">สามารถให้ลูกค้าสั่งอาหาร สั่งซ้ำ
+                                    หรือสั่งล่วงหน้าได้โดยไม่ต้องรอพนักงาน</dd>
+                            </div>
+                            <div class="relative pl-16">
+                                <dt class="text-base font-semibold leading-7 text-gray-900">
+                                    <span
+                                        class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-gray-300">&#9812;</span>
+
+                                    ตอบโจทย์ธุรกิจร้านอาหารและเครื่องดื่ม
+                                </dt>
+                                <dd class="mt-2 text-base leading-7 text-gray-600">QR ORDERING
+                                    เหมาะสำหรับธุรกิจอาหารและเครื่องดื่มทุกประเภท ตั้งแต่บูธร้านค้า ไปจนถึง คาเฟ่ บาร์ และ
+                                    ร้านอาหาร</dd>
+                            </div>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ล่าง-->
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 pt-10 ">
+        <div class="mx-auto max-w-2xl lg:text-center">
+            <h2 class="text-3xl font-semibold leading-7 text-indigo-600">ใช้ QR แล้วดีอย่างไร</h2>
+
+        </div>
+        <div class="py-12 relative overflow-hidden bg-white">
+            <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+                <div class="w-full flex flex-col items-end pr-16">
+
+                    <div class="h-full mt-auto overflow-hidden relative">
+                        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt="">
+                    </div>
+                </div>
+
+                <div
+                    class="py-20 bg-white relative before:absolute before:h-full before:w-screen  before:top-0 before:left-0">
+                    <div class="relative z-20 pl-12">
+                        <h2 class="text-2xl font-semibold leading-7 text-gray-900">แค่สแกน QR ก็เห็นเมนูและกดสั่งได้เลย
+                        </h2>
+                        <p class="mt-2 text-base leading-7 text-gray-600">
+                            <span>&#10003;</span> &nbsp; สร้างร้านค้าออนไลน์ง่ายๆ ภายในไม่กี่คลิก<br>
+                            <span>&#10003;</span> &nbsp; พิมพ์ QR ที่ออกแบบเอง ติดที่โต๊ะหรือเคาน์เตอร์ได้เลย<br>
+                            <span>&#10003;</span> &nbsp; QR Code ใช้งานได้ทุกอุปกรณ์<br>
+                            <span>&#10003;</span> &nbsp; สามารถสั่งซื้อแบบกลับบ้านและรับที่ร้านได้ล่วงหน้า<br>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-4  relative overflow-hidden bg-white">
+            <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+                <div
+                    class="py-20 bg-white relative before:absolute before:h-full before:w-screen  before:top-0 before:right-0">
+                    <div class="relative z-20 pl-12">
+                        <h2 class="text-2xl font-semibold leading-7 text-gray-900">สั่งปุ๊บก็เรียกเก็บเงินได้ปั๊บ
+                            แถมยังส่งออเดอร์ไปที่ครัวได้ทันที
+                        </h2>
+                        <p class="mt-2 text-base leading-7 text-gray-600">
+                            <span>&#10003;</span> &nbsp; ลูกค้าสามารถจัดการออเดอร์ของตัวเองได้อย่างเต็มที่<br>
+                            <span>&#10003;</span> &nbsp; จ่ายเงินได้ง่ายๆ ผ่านมือถือ<br>
+                            <span>&#10003;</span> &nbsp; ส่งใบเสร็จไปที่อีเมลของลูกค้าได้<br>
+                        </p>
+
+                    </div>
+                </div>
+                <div class="w-full flex flex-col pl-16">
+                    <div class="h-full mt-auto overflow-hidden relative">
+                        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="py-12 relative overflow-hidden bg-white">
+            <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+                <div class="w-full flex flex-col items-end pr-16">
+
+                    <div class="h-full mt-auto overflow-hidden relative">
+                        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt="">
+                    </div>
+                </div>
+
+                <div
+                    class="py-20 bg-white relative before:absolute before:h-full before:w-screen  before:top-0 before:left-0">
+                    <div class="relative z-20 pl-12">
+                        <h2 class="text-2xl font-semibold leading-7 text-gray-900">เมื่อออเดอร์พร้อมแล้ว
+                            ลูกค้าจะได้รับการแจ้งเตือนหรือเสิร์ฟที่โต๊ะได้ทันที
+                        </h2>
+                        <p class="mt-2 text-base leading-7 text-gray-600">
+                            <span>&#10003;</span> &nbsp; ลูกค้าสามารถไปรับเมื่อออเดอร์ถูกเรียก<br>
+                            <span>&#10003;</span> &nbsp; พนักงานสามารถเสิร์ฟที่โต๊ะอาหารที่โต๊ะได้เลยเช่นกัน<br>
+                            <span>&#10003;</span> &nbsp; ลูกค้าทานที่ร้านสามารถสั่งเพิ่มได้อย่างง่ายดายตามความสะดวก<br>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
