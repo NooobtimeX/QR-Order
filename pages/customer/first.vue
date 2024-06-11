115fe59b3d68c0ad611184d0021dc8622ee8f3eb
Tateung <156577615+Tateung@users.noreply.github.com>
2024-06-11 16:23:18 +0700

    call waiter

---
+++ pages/customer/first.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="grid grid-cols-1 gap-4">
+        <button @click="orderFood" class="bg-green-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-green-700">
+        สั่งอาหาร
+      </button>
+      <button @click="callStaff" class="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-700">
+        เรียกพนักงาน
+      </button>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    callStaff() {
+      alert('เรียกพนักงาน');
+    },
+    orderFood() {
+      alert('สั่งอาหาร');
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* คุณสามารถเพิ่มการสไตล์เพิ่มเติมได้ที่นี่ */
+</style>
